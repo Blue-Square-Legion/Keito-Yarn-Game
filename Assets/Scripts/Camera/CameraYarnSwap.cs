f78adbaa82e01fa0c461b6d5ff3d014988b35f7c
Brandon Jeon <62401369+BJeon04@users.noreply.github.com>
2024-07-17 21:44:25 -0400

    Camera Switching
    Worked on Camera switching script where the camera will switch from one yarn ball to another. Currently not working as the camera goes crazy.

--- Assets/Scripts/Camera/CameraYarnSwap.cs
+++ Assets/Scripts/Camera/CameraYarnSwap.cs
@@ -8,19 +8,28 @@ public class CameraYarnSwap : MonoBehaviour
 {
     [SerializeField] private new CinemachineFreeLook camera;
     [SerializeField] private GameObject focalpoint;
+    [SerializeField] private GameObject objects;
 
-    /* public void OnMouseEnter()
+     public void OnMouseEnter()
      {
-        // if (Input.GetKeyDown(KeyCode.E))
-        // {
-             camera.Follow = gameObject.transform;
-             camera.LookAt = gameObject.transform;
-        // }
-     }*/
+        if (Input.GetMouseButtonDown(1))
+        {
+            objects.transform.parent = this.transform;
+
+            camera.Follow = objects.transform;
+            camera.LookAt = objects.transform;
+
+            return;
+        }
+    }
     private void Start()
     {
         camera = FindObjectOfType<CinemachineFreeLook>();
         focalpoint = GameObject.Find("FocalPoint");
+        objects = GameObject.Find("Point");
+
+        
+
     }
 
 
@@ -32,15 +41,16 @@ public class CameraYarnSwap : MonoBehaviour
             camera.LookAt = focalpoint.transform;
             return;
         }
-
-        if (Input.GetMouseButtonDown(1))
+        OnMouseEnter();
+        
+        /*
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            camera.Follow = gameObject.transform;
-            camera.LookAt = gameObject.transform;
-           
-           
+            camera.Follow = objects.transform;
+            camera.LookAt = objects.transform;
             return;
-        }
+        }*/
+
     }
 
     
