1897cfa1d20e31112f74a03ec8947dc6987e2594
Robert Chaney <rwchaneyjr@yahoo.com>
2024-07-23 09:20:22 -0500

    Wind box added
    There is a windbox which was added it is a box which surrounds the scene when an object (ie yarn) collides with it it is given a direction and a force to go like thewind is affecting it.

---
+++ Assets/Scripts/Ball/Wind.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    [SerializeField] float _windForce = 10f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        var hitObj = other.gameObject;
+        if (hitObj != null)
+        {
+            var rb = hitObj.GetComponent<Rigidbody>();
+            var dir = transform.up;
+            rb.AddForce(dir * _windForce);
+        }
+    }
+}
