dce2c7122ac4c302212d0edaa8c7444cd69e7662
Robert Chaney <rwchaneyjr@yahoo.com>
2024-07-23 19:07:34 -0500

    Robert
    fan and wind tunnel

---
+++ Assets/Scripts/Ball/Wind.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Wind : MonoBehaviour
+{
+    [SerializeField] float _windForce = 10f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        var hitObj = other.gameObject;
+        if (hitObj != null)
+        {
+            var rb = hitObj.GetComponent<Rigidbody>();
+            var dir = transform.forward;
+            rb.AddForce(dir * _windForce);
+        }
+    }
+}
