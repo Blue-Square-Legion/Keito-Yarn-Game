791c02cffb3fab4833cbd479419261ea9dc7823b
Robert Chaney <rwchaneyjr@yahoo.com>
2024-07-30 19:27:10 -0500

    updated windbox

--- Assets/Scripts/Ball/Wind.cs
+++ Assets/Scripts/Ball/Wind.cs
@@ -5,37 +5,20 @@ using UnityEngine;
 public class Wind : MonoBehaviour
 {
     [SerializeField] float _windForce = 2f;
-    [SerializeField] public bool _up, _right,_forward,_left;
+    public Vector3 fanDirection = Vector3.right;
+
+
+
     private void OnTriggerStay(Collider other)
     {
         var hitObj = other.gameObject;
         if (hitObj != null)
         {
+           
+              
+                var rb = hitObj.GetComponent<Rigidbody>();
+                rb.AddForce(fanDirection * _windForce);
 
-            if (_up)
-            {
-                var dir = transform.up;
-                var rb = hitObj.GetComponent<Rigidbody>();
-                rb.AddForce(dir * _windForce);
-            } 
-            if(_right)
-            {
-                var dir = transform.right;
-                var rb = hitObj.GetComponent<Rigidbody>();
-                rb.AddForce(dir * _windForce);
-            }
-            if (_forward)
-            {
-                var dir = transform.right;
-                var rb = hitObj.GetComponent<Rigidbody>();
-                rb.AddForce(dir * _windForce);
-            }
-            if (_left)
-            {
-                var dir = transform.right;
-                var rb = hitObj.GetComponent<Rigidbody>();
-                rb.AddForce(dir * _windForce);
-            }
         }
     }
 }
