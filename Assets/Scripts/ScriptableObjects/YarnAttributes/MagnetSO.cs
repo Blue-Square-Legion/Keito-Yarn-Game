3ee224cd96dda13ab05bddb8e4541c62f8c5855a
seribeengton <seribeengton@gmail.com>
2024-08-27 23:07:35 -0400

    Additional updated to the magned SO
    Magnet SO is not functional at this time and is still being developed.

--- Assets/Scripts/ScriptableObjects/YarnAttributes/MagnetSO.cs
+++ Assets/Scripts/ScriptableObjects/YarnAttributes/MagnetSO.cs
@@ -6,18 +6,26 @@ using UnityEngine;
 public class MagnetSO : YarnBallEffectSO
 {
     private RaycastHit[] hits;
+    //private List<GameObject> yarnInRadius;
     [SerializeField] private float _magballRadius;
     [SerializeField] private float _pullForce, _pushForce;
+    public LayerMask layerMask;
+    //public 
     GameObject _ball;
 
+    private void Update() 
+    {
+        //yarnInRadius.Clear();
+    }
 
+    //ball is the game object the script is attached to. Target is the other ball the game object collides with. Not necesary for this script
     public override void CreateEffect(GameObject ball, Transform target = null)
     {
         _ball = ball;
     }
     public override void ApplyEffect(GameObject ball, Rigidbody ballRigidbody, Rigidbody targetRigidbody = null)
     {
-        hits = Physics.SphereCastAll(ball.transform.position, _magballRadius, Vector3.forward, 1f, 8);
+        hits = Physics.SphereCastAll(ball.transform.position, _magballRadius, Vector3.forward, 1f, layerMask);
     }
     public override bool ShouldApplyOnCollision()
     {
@@ -28,5 +36,18 @@ public class MagnetSO : YarnBallEffectSO
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(_ball.transform.position, _magballRadius);
+    }
+
+    IEnumerator ApplyMagnetism() 
+    {
+        foreach (RaycastHit yarnInRadius in hits) 
+        {
+            GameObject yarnBallHit = yarnInRadius.transform.gameObject;
+            if (yarnBallHit.GetComponent<Yarn>)
+            { 
+            
+            }
+        }
+        yield return null;
     }
 }
