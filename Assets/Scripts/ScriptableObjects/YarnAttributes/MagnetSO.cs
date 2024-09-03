d3dc5dd0bde8a945d6ba79ba8c9e731d0c9bbd49
seribeengton <seribeengton@gmail.com>
2024-09-02 22:40:29 -0400

    Additional Progress for MagnetEffect
    Created a working version of the Magnet Effect in a sepeare script, MagnetEffect.cs.
    This script works using a fixed update. There is a coroutine that could be initiated, but unable to test if this works atm because effects are only created on collision, but magnet effect should take place during instantiation

--- Assets/Scripts/ScriptableObjects/YarnAttributes/MagnetSO.cs
+++ Assets/Scripts/ScriptableObjects/YarnAttributes/MagnetSO.cs
@@ -10,44 +10,24 @@ public class MagnetSO : YarnBallEffectSO
     [SerializeField] private float _magballRadius;
     [SerializeField] private float _pullForce, _pushForce;
     public LayerMask layerMask;
-    //public 
-    GameObject _ball;
-
-    private void Update() 
-    {
-        //yarnInRadius.Clear();
-    }
+    private GameObject _ball;
 
     //ball is the game object the script is attached to. Target is the other ball the game object collides with. Not necesary for this script
     public override void CreateEffect(GameObject ball, Transform target = null)
     {
+        Debug.Log("Magnet Field Created");
         _ball = ball;
+        MagnetEffect MagEffect = new(_magballRadius, _pullForce, _pullForce);
+        MagEffect.StartCoroutine("CreateMagnetField");
+        MagEffect.OnDrawGizmos();
     }
     public override void ApplyEffect(GameObject ball, Rigidbody ballRigidbody, Rigidbody targetRigidbody = null)
     {
-        hits = Physics.SphereCastAll(ball.transform.position, _magballRadius, Vector3.forward, 1f, layerMask);
-    }
+        Debug.Log("Test");
+    } 
     public override bool ShouldApplyOnCollision()
     {
-        return true;
+        return false;
     }
 
-    private void OnDrawGizmoSelected()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(_ball.transform.position, _magballRadius);
-    }
-
-    IEnumerator ApplyMagnetism() 
-    {
-        foreach (RaycastHit yarnInRadius in hits) 
-        {
-            GameObject yarnBallHit = yarnInRadius.transform.gameObject;
-            if (yarnBallHit.GetComponent<Yarn>)
-            { 
-            
-            }
-        }
-        yield return null;
-    }
 }
