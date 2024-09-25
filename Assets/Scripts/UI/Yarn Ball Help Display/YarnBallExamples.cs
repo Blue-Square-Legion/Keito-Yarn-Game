c2d2ad6be5580a0bd3e030b7723f88a19dfa3f9c
XanferS <Funntmatthew@hotmail.com>
2024-09-25 16:14:05 -0400

    updated menu
    menu has updated descriptions and visuals including the cyan ball

--- Assets/Scripts/UI/Yarn Ball Help Display/YarnBallExamples.cs
+++ Assets/Scripts/UI/Yarn Ball Help Display/YarnBallExamples.cs
@@ -5,46 +5,54 @@ using UnityEngine.UI;
 
 public class YarnBallExamples : MonoBehaviour
 {
+    public Button blue;
     public Button red;
     public Button green;
-    public Button wildcard;
-    public Button change;
+    //public Button wildcard;
+    public Button cyan;
     public Button yellow;
     public Button back;
+    public GameObject blueImage;
     public GameObject redImage;
     public GameObject greenImage;
-    public GameObject wildcardImage;
+    //public GameObject wildcardImage;
     public GameObject changeImage;
     public GameObject yellowImage;
     // Start is called before the first frame update
     void Start()
     {
+        Button btnBlue = blue.GetComponent<Button>();
+        btnBlue.onClick.AddListener(blueHelp);
         Button btnRed = red.GetComponent<Button>();
         btnRed.onClick.AddListener(redHelp);
         Button btnGreen = green.GetComponent<Button>();
         btnGreen.onClick.AddListener(greenHelp);
-        Button btnWild = wildcard.GetComponent<Button>();
-        btnWild.onClick.AddListener(wildcardHelp);
-        Button btnChange = change.GetComponent<Button>();
-        btnChange.onClick.AddListener(changeHelp);
+        //Button btnWild = wildcard.GetComponent<Button>();
+        //btnWild.onClick.AddListener(wildcardHelp);
+        Button btnChange = cyan.GetComponent<Button>();
+        btnChange.onClick.AddListener(cyanHelp);
         Button btnYellow = yellow.GetComponent<Button>();
         btnYellow.onClick.AddListener(yellowHelp);
         Button btnBack = back.GetComponent<Button>();
         btnBack.onClick.AddListener(BackMenu);
     }
-    void redHelp()
+    void blueHelp() 
     {
         yarnMenu(1);
     }
-    void greenHelp()
+    void redHelp()
     {
         yarnMenu(2);
     }
-    void wildcardHelp()
+    void greenHelp()
     {
         yarnMenu(3);
     }
-    void changeHelp()
+    //void wildcardHelp()
+    //{
+    //    yarnMenu(3);
+    //}
+    void cyanHelp()
     {
         yarnMenu(4);
     }
@@ -62,49 +70,49 @@ public class YarnBallExamples : MonoBehaviour
         {
             case 0:
                 Debug.Log("Back selected");
+                blueImage.SetActive(false);
                 redImage.SetActive(false);
                 greenImage.SetActive(false);
-                wildcardImage.SetActive(false);
                 changeImage.SetActive(false);
                 yellowImage.SetActive(false);
                 break;
             case 1:
                 Debug.Log("Red ball selected");
-                redImage.SetActive(true);
+                blueImage.SetActive(true);
+                redImage.SetActive(false);
                 greenImage.SetActive(false);
-                wildcardImage.SetActive(false);
                 changeImage.SetActive(false);
                 yellowImage.SetActive(false);
                 break;
             case 2:
                 Debug.Log("Green ball selected");
-                redImage.SetActive(false);
-                greenImage.SetActive(true);
-                wildcardImage.SetActive(false);
+                blueImage.SetActive(false);
+                redImage.SetActive(true);
+                greenImage.SetActive(false);
                 changeImage.SetActive(false);
                 yellowImage.SetActive(false);
                 break;
             case 3:
                 Debug.Log("Wildcard ball selected");
+                blueImage.SetActive(false);
                 redImage.SetActive(false);
-                greenImage.SetActive(false);
-                wildcardImage.SetActive(true);
+                greenImage.SetActive(true);
                 changeImage.SetActive(false);
                 yellowImage.SetActive(false);
                 break;
             case 4:
                 Debug.Log("Change ball selected");
+                blueImage.SetActive(false);
                 redImage.SetActive(false);
                 greenImage.SetActive(false);
-                wildcardImage.SetActive(false);
                 changeImage.SetActive(true);
                 yellowImage.SetActive(false);
                 break;
             case 5:
                 Debug.Log("Yellow ball selected");
+                blueImage.SetActive(false);
                 redImage.SetActive(false);
                 greenImage.SetActive(false);
-                wildcardImage.SetActive(false);
                 changeImage.SetActive(false);
                 yellowImage.SetActive(true);
                 break;
