7dee5b94812d795847f338126aec5a465b6e9334
Louie Marcou <58005859+LouieMarcou@users.noreply.github.com>
2024-10-14 13:47:17 -0500

    Adding particles (#111)
    * Added new models
    
    * Add magnetic particles
    
    * added particles to ball
    
    * Matched Magnetic field size

--- Assets/Scripts/ScriptableObjects/YarnAttributes/MagnetEffectSO.cs
+++ Assets/Scripts/ScriptableObjects/YarnAttributes/MagnetEffectSO.cs
@@ -7,6 +7,8 @@ public class MagnetEffectSO : YarnBallEffectSO
     [SerializeField] private float _pullForce, _pushForce;
     [SerializeField] private float _effectDuration = 5f; // Duration for which the effect is active
     public LayerMask layerMask;
+    [SerializeField] private GameObject particlesPrefab;
+
 
     public override void CreateEffect(GameObject ball, Transform target = null)
     {
@@ -20,7 +22,7 @@ public class MagnetEffectSO : YarnBallEffectSO
         }
 
         Renderer ballRenderer = ball.GetComponent<Renderer>();
-        handler.Initialize(_magballRadius, _pullForce, _pushForce, _effectDuration, layerMask, ballRenderer);
+        handler.Initialize(_magballRadius, _pullForce, _pushForce, _effectDuration, layerMask, ballRenderer, particlesPrefab);
     }
 
     public override void ApplyEffect(GameObject ball, Rigidbody ballRigidbody, Rigidbody targetRigidbody = null)
